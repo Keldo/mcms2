3f0937c126a2695a9f56ef5bf40afb694ec61fc2
keldo <scilley@outlook.com>
2019-10-05 21:31:06 -0400

    added is_ssl to core footer

--- themes/core/footer.php
+++ themes/core/footer.php
@@ -10,32 +10,38 @@
   // Social Media
   if(FACEBOOK != "")
   {
-	  echo '<a href="https://www.facebook.com/'.FACEBOOK.'" target="_blank"><img src="/themes/'.THEME.'/img/social_media/facebook.png" alt="'.SITENAME.' on Facebook"/></a>&nbsp;';
+	  echo '<a href="https://www.facebook.com/'.FACEBOOK.'" target="_blank"><img src="img/social_media/facebook.png" alt="'.SITENAME.' on Facebook"/></a>&nbsp;';
   }
   if(TWITTER != "")
   {
-	  echo '<a href="https://www.twitter.com/'.TWITTER.'" target="_blank"><img src="/themes/'.THEME.'/img/social_media/twitter.png" alt="'.SITENAME.' on Twitter"/></a>&nbsp;';
+	  echo '<a href="https://www.twitter.com/'.TWITTER.'" target="_blank"><img src="img/social_media/twitter.png" alt="'.SITENAME.' on Twitter"/></a>&nbsp;';
   }
   
   // Sentry Image
   if(file_exists("app/sentry/security.php"))
   {
-      echo '<img src="/themes/'.THEME.'/img/security/sentry.png" height="30" width="80" alt="This site is protected by Sentry" />&nbsp;';
+      echo '<img src="/img/security/sentry.png" height="30" width="80" alt="This site is protected by Sentry" />&nbsp;';
+  }
+  if($core->is_ssl() == true)
+  {
+	  echo '<img src="/themes/'.THEME.'/img/ssl.png" height="30" width="80" alt="This site is Secure!" />&nbsp;';
   }
   
   // Sindication
   if(RSS == 1)
   {
-	  echo '<a href="/rss" /><img src="/themes/'.THEME.'/img/rss.png" alt="'.SITENAME.' News Feed"/></a>&nbsp;';
+	  echo '<a href="/rss" /><img src="img1/rss.png" alt="'.SITENAME.' News Feed"/></a>&nbsp;';
   }
   ?>
    </div>
 <?php
-   $query = "SELECT * FROM site_views";
+
+ $query = "SELECT * FROM site_views";
 $result = $db->query($query);
 $row = $db->fetch_assoc($result);
 $count = $row['site_count'];
 echo '<center><small>This site has been viewed '.$count.' times</small></center>';
+
 ?>
 </div>
 </div>
