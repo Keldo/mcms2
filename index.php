febe65ea9201238b5460e146678f890c35ce18de
keldo <scilley@outlook.com>
2019-10-12 23:04:02 -0400

    index

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
 <?php
 	require_once('app/autoload.php');
-	
 	use database\db;
+	use wheel\wheel;
 	use microcms\core;
+	use microcms\blogs;
+	use microcms\settings;
 	use sentry\security;
-        
-        // Instantiate the Database Class
 	$db = new db();
-
-        // Instantiate the Core Class
 	$core = new core();
-
-        // Instantiate the Sentry Class
 	$sentry = new security();
-	
+	$season = new wheel();
+	$seo = new settings();
+	$settings = new settings();
 	// Function operatives
 	if (isset($_GET['p']))
 	{
@@ -27,56 +25,89 @@
 	{
 		$d = NULL;
 	}
-
 	// The Page Switch
 	switch($d)
 	{
 		case 'error':
 		$content = 'modules/error/main.php';
 		break;
-	
+		case 'microcms':
+		$content = 'modules/microcms/main.php';
+		break;
+		case 'sentry':
+		$content = 'modules/sentry/main.php';
+		break;
+		case 'blogs':
+		$content = 'modules/blogs/main.php';
+		break;
+		case 'cart':
+		$content = 'modules/cart/main.php';
+		break;
+		case 'donate':
+		$content = 'modules/donate/main.php';
+		break;
+		case 'user':
+		$content = 'modules/user/main.php';
+		break;
+		case 'manage':
+		$content = 'modules/manage/main.php';
+		break;
+		case 'login':
+		$content = 'modules/manage/login.php';
+		break;
+		case 'logout':
+		$content = 'modules/manage/logout.php';
+		break;
+		case 'manage':
+		$content = 'modules/manage/main.php';
+		break;
 		default:
 		$content = 'modules/main.php';
 		break;
 	}
-	
-	/*
-	 * Prevention Session Injection
-	*/
-	if (isset($_REQUEST['_SESSION'])) die("Get lost Muppet!");
-
-        /*
-	 * Check to ensure that the config file exist, if not, exit
-	 * In the future, this file will be created during install
-	*/
 	if(file_exists('config/config.php'))
 	{
 		// We need the Visitors IP Address
 		$ip = $sentry->get_real_ip();
-		
 		// Is this IP address banned?
 		$sentry->check_ban($ip);
-		
 		// Ban as needed from threat countries
 		$country = $sentry->ip_location($ip);
 		echo '<!-- Your current country location: '.$country.'-->';
-		
-		// Include the Current theme header
+		$google_verification = $seo->google_verification;
+		$google_analytics_id = $seo->google_analytics_id;
+		$bing_verification = $seo->bing_verification;
+		$facebook = $seo->facebook;
+		$twitter = $seo->twitter;
+		$instagram = $seo->instagram;
+		$youtube = $seo->youtube;
+		$site_name = $settings->site_name;
+		$base_url = $settings->base_url;
+		$site_logo = $settings->site_logo;
+		$description = $settings->description;
+		$keywords = $settings->keywords;
+		// Include the theme's Header
 		include 'themes/'.THEME.'/header.php';
-
 		// Error Checking
 		$core->check_errors(1);
-
+		// Session Time Out
+		//$core->log_timer(900);
 		// Set the Desired Time Zone
 		date_default_timezone_set(TIMEZONE);
-		
-		// Load the Content
+		/*
+		 * Prevention Session Injection
+		 */
+		if (isset($_REQUEST['_SESSION']))
+		{
+			die("Get lost Muppet!");
+		}
+		// Include content from each module
 		include $content;
-		
-		// Include the current theme footer
+		// Include the themes Footer
 		include 'themes/'.THEME.'/footer.php';
 	}
 	else
 	{
 		echo 'This site is not Installed or Configured Correctly';
 	}
+?>
